1c37d84f489bb7a8d16f9725f84898e838e0b734
DenisD <dionisvl3@gmail.com>
2026-02-10 22:30:23 +0300

    cache control fix; lazy-loading fix;

--- api-symfony/public/js/lazy-load.js
+++ api-symfony/public/js/lazy-load.js
@@ -21,6 +21,29 @@ class LazyImageLoader {
     loadImage(img) {
         const src = img.dataset.src;
         const srcset = img.dataset.srcset;
+        const placeholder = img.parentElement?.querySelector('.lazy-placeholder');
+
+        // Pre-check cache BEFORE setting src on tempImg
+        const cacheTest = new Image();
+        cacheTest.src = src;
+
+        const isCached = cacheTest.complete && cacheTest.naturalWidth > 0;
+
+        if (isCached) {
+            // Image in cache - show instantly without animation
+            img.src = src;
+            if (srcset) {
+                img.srcset = srcset;
+            }
+            img.removeAttribute('data-src');
+            img.removeAttribute('data-srcset');
+            img.style.opacity = '1';
+            img.style.transition = 'none';
+            placeholder?.remove();
+            return;
+        }
+
+        // Not cached - proceed with normal lazy load animation
         const tempImg = new Image();
 
         tempImg.onload = () => {
@@ -30,8 +53,6 @@ class LazyImageLoader {
             }
             img.removeAttribute('data-src');
             img.removeAttribute('data-srcset');
-
-            const placeholder = img.parentElement?.querySelector('.lazy-placeholder');
 
             requestAnimationFrame(() => {
                 img.classList.add('loaded');
@@ -45,6 +66,7 @@ class LazyImageLoader {
 
         tempImg.onerror = () => {
             img.classList.add('load-error');
+            placeholder?.remove();
         };
 
         tempImg.src = src;
